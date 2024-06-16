2de26a2d60cc3adc308cb5480df97ccac4878485
erika_cats <scenery@student.ubc.ca>
2024-06-15 18:29:50 -0700

    Add doctor login page

--- android/app/src/main/java/com/pcos_app/MainActivity.kt
+++ android/app/src/main/java/com/pcos_app/MainActivity.kt
@@ -2,21 +2,20 @@ package com.pcos_app
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
-import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.facebook.react.ReactRootView
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView
 
 class MainActivity : ReactActivity() {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "pcos_app"
+    override fun getMainComponentName(): String? {
+        return "pcos_app"
+    }
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    override fun createReactActivityDelegate(): ReactActivityDelegate {
+        return object : ReactActivityDelegate(this, getMainComponentName()) {
+            override fun createRootView(): ReactRootView {
+                return RNGestureHandlerEnabledRootView(this@MainActivity)
+            }
+        }
+    }
 }
---
+++ src/components/DoctorPortal/Button.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+type ButtonProps = {
+  title: string;
+  onPress: () => void;
+};
+
+const Button: React.FC<ButtonProps> = ({ title, onPress }) => {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#E57373',
+    width: 220,
+    padding: 15,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default Button;
